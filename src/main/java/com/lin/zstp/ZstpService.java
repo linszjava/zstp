52426228f753fc8f36c74265ac6308847fa38604
linsz <linszjava@gmail.com>
2024-10-04 16:20:23 +0800

    初始测试 通过

---
+++ src/main/java/com/lin/zstp/ZstpService.java
@@ -0,0 +1,19 @@
+package com.lin.zstp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author linsz
+ * @version v1.0
+ * @date 2024/10/4 14:21
+ */
+@SpringBootApplication
+public class ZstpService {
+    public static void main(String[] args) {
+        SpringApplication.run(ZstpService.class, args);
+        System.out.println("===========>>>>>>>ZstpService started==========>");
+    }
+}
